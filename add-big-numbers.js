750f5871cc249ff360b7aaa0e8077bb9656b16fb
Min Thu Kyaw <helloeleven968@gmail.com>
2024-04-29 15:45:35 +0630

    feat(app): add adding-big-int

---
+++ add-big-numbers.js
@@ -0,0 +1,26 @@
+function add(A, B) {
+    const AL = A.length
+    const BL = B.length
+    const ML = Math.max(AL, BL)
+  
+    let carry = 0, sum = ''
+  
+    for (let i = 1; i <= ML; i++) {
+      let a = +A.charAt(AL - i)
+      let b = +B.charAt(BL - i)
+  
+  
+      let t = carry + a + b
+      carry = t/10 |0
+      t %= 10
+
+      sum = (i === ML && carry)
+        ? carry*10 + t + sum
+        : t + sum
+
+    }
+    return sum
+  }
+  
+
+ add('987', '234')
